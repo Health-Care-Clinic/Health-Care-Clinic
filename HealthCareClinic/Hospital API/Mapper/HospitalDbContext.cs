0f41025793e1a7fd1bbbff005ef7b425a2c6f503
Marija <marijap@protonmail.com>
2021-10-29 20:42:37 +0200

    chore: add initial project

---
+++ HealthCareClinic/Hospital API/Mapper/HospitalDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital_API.Mapper
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options) { }
+    }
+}
