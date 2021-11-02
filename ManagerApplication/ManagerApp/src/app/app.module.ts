c809b04427bb473ea76608e7a5efdc1d791cd06c
MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>
2021-11-02 17:17:38 +0100

    feat(e02): implement front for drawing hospital map

--- ManagerApplication/ManagerApp/src/app/app.module.ts
+++ ManagerApplication/ManagerApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HospitalMapComponent } from './component/hospital-map/hospital-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalMapComponent
   ],
   imports: [
     BrowserModule,
