816493aec34bfaa9e944c75344806dd1fcfb47ee
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2021-11-03 18:25:17 +0100

    chore: add initial pharmacy project

---
+++ Pharmacy API/Pharmacy API/Mapper/PharmacyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy_API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_API.Mapper
+{
+    public class PharmacyDbContext: DbContext
+    {
+        public DbSet<Medicine> Medicines { get; set; }
+
+        public PharmacyDbContext(DbContextOptions<PharmacyDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medicine>().HasData(
+                new Medicine { Id = 1, Name = "Brufen", Quantity = 400},
+                new Medicine { Id = 2, Name = "Klacid", Quantity = 200 },
+                new Medicine { Id = 3, Name = "Paracetamol", Quantity = 250 }
+                );
+        }
+    }
+}
---
+++ Pharmacy API/Pharmacy API/Migrations/20211103144144_FirstMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Pharmacy_API.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicines", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medicines",
+                columns: new[] { "Id", "Name", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Brufen", 400 },
+                    { 2, "Klacid", 200 },
+                    { 3, "Paracetamol", 250 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicines");
+        }
+    }
+}
