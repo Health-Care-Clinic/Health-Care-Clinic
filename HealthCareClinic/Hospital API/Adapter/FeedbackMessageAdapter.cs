819ff992b8805dc21a5651621a0d401663a100ef
Darijan Mićić <darijan.micic10@gmail.com>
2021-11-04 14:10:41 +0100

    feat(p02): add api controller and adapter
    API controller contains one method for retrieving all data from table. Adapter class is used for conversion from date and time to string (and vice versa). This conversion is needed for HTTP communication between Web API project and Angular front-end application.

---
+++ HealthCareClinic/Hospital API/Adapter/FeedbackMessageAdapter.cs
@@ -0,0 +1,68 @@
+﻿using ClinicCore.Model;
+using Hospital_API.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital_API.Adapter
+{
+    public class FeedbackMessageAdapter
+    {
+        public static FeedbackMessage FeedbackMessageDTOToFeedbackMessage(FeedbackMessageDTO dto)
+        {
+            FeedbackMessage feedbackMessage = new FeedbackMessage();
+
+            feedbackMessage.Id = dto.Id;
+            feedbackMessage.Date = ConvertToDate(dto.DateAsString);
+            feedbackMessage.Text = dto.Text;
+            feedbackMessage.IsAnonymous = dto.IsAnonymous;
+            feedbackMessage.Identity = dto.Identity;
+            feedbackMessage.CanBePublished = dto.CanBePublished;
+            feedbackMessage.IsPublished = dto.IsPublished;
+
+            return feedbackMessage;
+        }
+
+        public static FeedbackMessageDTO FeedbackMessageToFeedbackMessageDTO(FeedbackMessage feedbackMessage)
+        {
+            FeedbackMessageDTO dto = new FeedbackMessageDTO();
+
+            dto.Id = feedbackMessage.Id;
+            dto.DateAsString = ConvertToString(feedbackMessage.Date);
+            dto.Text = feedbackMessage.Text;
+            dto.IsAnonymous = feedbackMessage.IsAnonymous;
+            dto.Identity = feedbackMessage.Identity;
+            dto.CanBePublished = feedbackMessage.CanBePublished;
+            dto.IsPublished = feedbackMessage.IsPublished;
+
+            return dto;
+        }
+
+        private static DateTime ConvertToDate(String feedbackDateAsString)
+        {
+            String[] mainParts = feedbackDateAsString.Split(' ');
+
+            String[] dateParts = mainParts[0].Split('/');
+            int year = int.Parse(dateParts[0]);
+            int month = int.Parse(dateParts[1]);
+            int day = int.Parse(dateParts[2]);
+
+            String[] timeParts = mainParts[1].Split(':');
+            int hour = int.Parse(timeParts[0]);
+            int minute = int.Parse(timeParts[1]);
+            int second = int.Parse(timeParts[2]);
+
+            DateTime feedbackDate = new DateTime(year, month, day, hour, minute, second);
+
+            return feedbackDate;
+        }
+
+        private static String ConvertToString(DateTime feedbackDate)
+        {
+            String feedbackDateAsString = feedbackDate.ToString();
+
+            return feedbackDateAsString;
+        }
+    }
+}
---
+++ HealthCareClinic/Hospital API/Controller/FeedbackMessagesController/FeedbackMessageController.cs
@@ -0,0 +1,32 @@
+﻿using ClinicCore.Model;
+using Hospital.Mapper;
+using Hospital_API.Adapter;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital_API.Controller.FeedbacksController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackMessageController : ControllerBase
+    {
+        private readonly HospitalDbContext dbContext;
+
+        public FeedbackMessageController(HospitalDbContext context)
+        {
+            dbContext = context;
+        }
+
+        [HttpGet]       // GET /api/feedbackMessage
+        public IActionResult Get()
+        {
+            List<FeedbackMessage> result = new List<FeedbackMessage>();
+            dbContext.FeedbackMessages.ToList().ForEach(feedbackMessage => result.Add(feedbackMessage));
+            return Ok(result);
+        }
+    }
+}
