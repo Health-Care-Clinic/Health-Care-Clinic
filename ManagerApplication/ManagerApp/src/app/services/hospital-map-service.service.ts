70bdbea1a5bb473cbf8b49e2bc88b44be27a9ba8
AleksandarSavicRA8-2018 <73191277+AleksandarSavicRA8-2018@users.noreply.github.com>
2021-11-04 19:59:27 +0100

    feat(e03):changing floors front
    * feat(e02): implement front for drawing hospital map
    
    * feat(e03): implement floor changing
    
    * improvement(e03): improve element aligment
    
    * improvement(e03): add back button to hospital complex map from floors
    
    Co-authored-by: MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>
    Co-authored-by: VladimirJ123 <73643698+VladimirJ123@users.noreply.github.com>

--- ManagerApplication/ManagerApp/src/app/services/hospital-map-service.service.ts
+++ ManagerApplication/ManagerApp/src/app/services/hospital-map-service.service.ts
@@ -3,6 +3,8 @@ import { Building, TypeOfBuilding } from '../model/building';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { Floor } from '../model/floor';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,10 +21,11 @@ export class HospitalMapService {
 
   public getBuildings(): Observable<Array<Building>> {
     let buildings: any;
-    let building1 = new Building(1,260,30,400,140,"Building1",TypeOfBuilding.Hospital,null);
-    let building2 = new Building(2,260,230,400,140,"Building2",TypeOfBuilding.Hospital,null);
-    let building3 = new Building(3,30,130,180,240,"Building3",TypeOfBuilding.Hospital,null);
-    let building4 = new Building(4,700,230,300,140,"Building4",TypeOfBuilding.Hospital,null);
+    let building1 = new Building(1,260,30,400,140,"Building1",TypeOfBuilding.Hospital,[new Floor(1, "Sprat 1", null), new Floor(2, "Sprat 2", null), new Floor(3, "Sprat 3", null)]);
+    let building2 = new Building(2,260,230,400,140,"Building2",TypeOfBuilding.Hospital,[new Floor(4, "Sprat 1", null), new Floor(5, "Sprat 2", null)]);
+    let building3 = new Building(3,30,130,180,240,"Building3",TypeOfBuilding.Hospital,[new Floor(6, "Sprat 1", null), new Floor(7, "Sprat 2", null), new Floor(8, "Sprat 3", null)]);
+    let building4 = new Building(4,700,230,300,140,"Building4",TypeOfBuilding.Hospital,[new Floor(9, "Sprat 1", null), new Floor(10, "Sprat 2", null)]);
+
     let building5 = new Building(5,30,30,200,70,"Parking1",TypeOfBuilding.Parking,null);
     let building6 = new Building(6,800,30,200,70,"Parking2",TypeOfBuilding.Parking,null);
     let building7 = new Building(7,800,130,200,70,"Parking3",TypeOfBuilding.Parking,null);
