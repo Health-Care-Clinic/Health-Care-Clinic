e0b895f81dbc16d6a9791a874272ad52657117d0
GGmaz <kajtes.nikola@gmail.com>
2021-11-05 21:53:37 +0100

    feat(p05):design landing page (#10)
    * feat(p05): imported material and started working on design
    
    * improvement(p05): add header
    
    * style(p05): add information about us

--- PatientApplication/PatientApp/src/app/app.module.ts
+++ PatientApplication/PatientApp/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';   //da bih mogao da koristim 2-way binding sa ngModel
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
+
+const MaterialComponents = [
+  MatSliderModule,
+  MatToolbarModule,
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialComponents
   ],
   providers: [],
   bootstrap: [AppComponent]
