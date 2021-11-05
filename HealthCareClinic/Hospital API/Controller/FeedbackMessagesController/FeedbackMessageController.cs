15df2cf527868e8e26b8b0f79cb82654234926e9
Darijan Mićić <darijan.micic10@gmail.com>
2021-11-05 11:23:32 +0100

    feat(p02): add method for retrieving publishable feedbacks in controller
    Controller methods are now returning "List<FeedbackMessageDTO>".

--- HealthCareClinic/Hospital API/Controller/FeedbackMessagesController/FeedbackMessageController.cs
+++ HealthCareClinic/Hospital API/Controller/FeedbackMessagesController/FeedbackMessageController.cs
@@ -1,6 +1,7 @@
 ﻿using ClinicCore.Model;
 using Hospital.Mapper;
 using Hospital_API.Adapter;
+using Hospital_API.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -24,8 +25,19 @@ namespace Hospital_API.Controller.FeedbacksController
         [HttpGet]       // GET /api/feedbackMessage
         public IActionResult Get()
         {
-            List<FeedbackMessage> result = new List<FeedbackMessage>();
-            dbContext.FeedbackMessages.ToList().ForEach(feedbackMessage => result.Add(feedbackMessage));
+            List<FeedbackMessageDTO> result = new List<FeedbackMessageDTO>();
+            dbContext.FeedbackMessages.ToList().ForEach(feedbackMessage 
+                => result.Add(FeedbackMessageAdapter.FeedbackMessageToFeedbackMessageDTO(feedbackMessage)));
+            return Ok(result);
+        }
+
+        [HttpGet("publishable")]       // GET /api/feedbackMessage/publishable
+        public IActionResult GetPublishable()
+        {
+            List<FeedbackMessage> feedbackMessages = dbContext.FeedbackMessages.Where(feedbackMessage => feedbackMessage.CanBePublished == true).ToList();
+            List<FeedbackMessageDTO> result = new List<FeedbackMessageDTO>();
+            feedbackMessages.ForEach(FeedbackMessage 
+                => result.Add(FeedbackMessageAdapter.FeedbackMessageToFeedbackMessageDTO(FeedbackMessage)));
             return Ok(result);
         }
     }
