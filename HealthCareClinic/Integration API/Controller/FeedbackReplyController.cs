94188182603f28ace9dd60b0dcb2bb375850b1f6
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2021-11-06 22:59:24 +0100

    feat(i05): implement feedback reply for pharmacy

---
+++ HealthCareClinic/Integration API/Controller/FeedbackReplyController.cs
@@ -0,0 +1,47 @@
+﻿using Integration;
+using Integration.Pharmacy.Model;
+using Integration_API.Adapter;
+using Integration_API.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_API.Controller
+{
+    [Route("hospital/[controller]")]
+    [ApiController]
+    public class FeedbackReplyController : ControllerBase
+    {
+        private readonly IntegrationDbContext _dbContext;
+
+        public FeedbackReplyController(IntegrationDbContext integrationDbContext)
+        {
+            _dbContext = integrationDbContext;
+        }
+
+        [HttpPost("receive")]
+        public IActionResult ReceiveFeedbackReply(FeedbackReplyDTO dto)
+        {
+            if (dto.Text.Length <= 0)
+            {
+                return BadRequest();
+            }
+
+            FeedbackReply receivedFeedbackReply = FeedbackReplyAdapter.FeedbackReplyDtoToFeedbackReply(dto);
+
+            _dbContext.FeedbackReplies.Add(receivedFeedbackReply);
+            _dbContext.SaveChanges();
+
+            return Ok("success");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("radi");
+        }
+    }
+}
