f82a188562b5a4a5c5645311fe2b93cf2a7d0a21
GGmaz <kajtes.nikola@gmail.com>
2021-11-07 13:15:00 +0100

    improvement(p05): design landing page
    * improvement(p05): add feedback view
    
    * style(p05): add image slider

--- PatientApplication/PatientApp/src/app/landing-page/landing-page.component.ts
+++ PatientApplication/PatientApp/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +7,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  slides = [
+    {
+      image: /* "../images/slide1.jpg" */
+        "https://images.pexels.com/photos/3769151/pexels-photo-3769151.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/127873/pexels-photo-127873.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/2383010/pexels-photo-2383010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/3985163/pexels-photo-3985163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    }
+  ];
 
-  constructor() { }
+  feedback = {
+    user: "",
+    content: ""
+  }
+
+  feedbacks = [
+      {
+        user: "Nadja",
+        content: "adasdas"
+      },
+      {
+        user: "Nadja",
+        content: "adasdas"
+      },
+      {
+        user: "Nadja",
+        content: "adasdas"
+      },
+      {
+        user: "Nadja",
+        content: "adasdas"
+      },
+      {
+        user: "Nadja",
+        content: "adasdas"
+      },
+      {
+        user: "Nadja",
+        content: "adasdas"
+      },
+      {
+        user: "Nadja",
+        content: "adasdas"
+      }
+  ];
+
+  constructor(public dialog: MatDialog) { 
+  }
 
   ngOnInit(): void {
   }
 
+  openFeedbackDialog(feedback: any): void {
+    
+  }
 }
