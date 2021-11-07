efbbe3428fdc86b694572eb21c76f57b6547919c
anagrahovac <grahovac.ana99@gmail.com>
2021-11-07 17:12:27 +0100

    feat(i01): added component for pharmacy registation in ManagerApplication

--- ManagerApplication/ManagerApp/src/app/app.module.ts
+++ ManagerApplication/ManagerApp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PharmacyRegistrationComponent } from './registration/pharmacy-registration/pharmacy-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PharmacyRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ManagerApplication/ManagerApp/src/app/registration/pharmacy-registration/pharmacy-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'registration',
+  templateUrl: './pharmacy-registration.component.html',
+  styleUrls: ['./pharmacy-registration.component.css']
+})
+export class PharmacyRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
