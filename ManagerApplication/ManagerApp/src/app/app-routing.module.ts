8da1782d961f2d791c0acc60fa03b8490c4c75e4
Darijan Mićić <73229338+Darijan-Micic-RA-203-2017@users.noreply.github.com>
2021-11-08 20:14:31 +0100

    feat(p02): implement front-end for viewing all feedbacks
    Manager now can view all submitted feedbacks.
    
    Co-authored-by: GGmaz <kajtes.nikola@gmail.com>

--- ManagerApplication/ManagerApp/src/app/app-routing.module.ts
+++ ManagerApplication/ManagerApp/src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
+import { FeedbackViewComponent } from './component/feedback/feedback-view/feedback-view.component';
 import { PharmacyRegistrationComponent } from './registration/pharmacy-registration/pharmacy-registration.component';
 
 const routes: Routes = [
-  {
-    path: 'registration',
-    component: PharmacyRegistrationComponent
-  }
+  { path: 'registration',  component: PharmacyRegistrationComponent },
+  { path: 'feedback-view', component: FeedbackViewComponent}
 ];
 
 @NgModule({
--- ManagerApplication/ManagerApp/src/app/app.module.ts
+++ ManagerApplication/ManagerApp/src/app/app.module.ts
@@ -10,28 +10,53 @@ import { AppComponent } from './app.component';
 import { PharmacyRegistrationComponent } from './registration/pharmacy-registration/pharmacy-registration.component';
 import { HospitalMapComponent } from './component/hospital-map/hospital-map.component';
 import { FloorComponent } from './component/hospital-map/floor.component';
+import { FeedbackViewComponent } from './component/feedback/feedback-view/feedback-view.component';
 import { RouterModule } from '@angular/router';
+import { FeedbackService } from './services/feedback.service';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule  } from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+
+
+const MaterialComponents = [
+  MatSliderModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatTableModule
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PharmacyRegistrationComponent,
     HospitalMapComponent,
-    FloorComponent
+    FloorComponent,
+    FeedbackViewComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     FormsModule,
-    HttpClientModule,
     CommonModule,
     RouterModule.forRoot([
       { path: 'registration', component: PharmacyRegistrationComponent},
       { path: 'floor/:idb/:idf', component: FloorComponent },
       { path: '', component: HospitalMapComponent }
-    ])
+    ]),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialComponents
   ],
-  providers: [],
+  providers: [FeedbackService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
