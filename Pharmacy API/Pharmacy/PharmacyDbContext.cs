e912372d4aba5f431bb1ea1c9b12327844379ae9
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2021-11-16 10:48:18 +0100

    chore(d04): update dummy data

--- Pharmacy API/Pharmacy/PharmacyDbContext.cs
+++ Pharmacy API/Pharmacy/PharmacyDbContext.cs
@@ -19,9 +19,9 @@ namespace Pharmacy
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Medicine>().HasData(
-                new Medicine { Id = 1, Name = "Brufen", Quantity = 400 },
-                new Medicine { Id = 2, Name = "Klacid", Quantity = 200 },
-                new Medicine { Id = 3, Name = "Paracetamol", Quantity = 250 }
+                new Medicine { Id = 1, Name = "Brufen", Quantity = 400, Manufacturer = "Galenika", Usage = "Lek BRUFEN se može kratkotrajno upotrebljavati u terapiji bolnih stanja poput zubobolje, bolova nakon operativnih zahvata, bolnih menstruacija i glavobolje (uključujući migrenu).Brufen može da se koristi kod povreda mekih tkiva kao što su uganuća i istegnuća.Aktivna supstanca leka BRUFEN je ibuprofen i svaka tableta sadrži 200 mg ibuprofena", Weight = 200, SideEffects = { }, Reactions = { }, CompatibileMedicine = { } },
+                new Medicine { Id = 2, Name = "Klacid", Quantity = 200, Manufacturer = "Galenika", Usage = "Klacid tablete se koriste za lečenje infekcija kao što su infekcije donjih disajnih puteva pluća, kao što je bronhitis i zapaljenje pluća, infekcije gornjih disajnih puteva kao što je infekcija grla i sinusa, infekcije kože i mekih tkiva, infekcije Helicobacter pylori kod pacijenata sa čirom na dvanaestopalačnom crevu Lek Klacid MR namenjen je odraslima i deci", Weight = 200, SideEffects = { }, Reactions = { }, CompatibileMedicine = { } },
+                new Medicine { Id = 3, Name = "Paracetamol", Quantity = 250, Manufacturer = "Galenika", Usage = "Paracetamol je blag analgetik i antipiretik. Upotreba tableta se preporučuje za lečenje bolnih i febrilnih stanja kao što su glavobolja(uključujući i migrenu i tenzionu glavobolju), zubobolja, bol u leđima, reumatski, bolovi i bolovi u mišićima, dismenoreja, bol u grlu, kao i za ublažavanje groznice, odnosno tegoba i bolova koji prate prehladu i grip.Lek se takođe može koristiti kao simptomatska terapija kod bolova povezanih sa osteoartritisom", Weight = 200, SideEffects = { }, Reactions = { }, CompatibileMedicine = { } }
             );
 
             modelBuilder.Entity<ApiKey>()
