951b6e854b9ffac0049e4ad59debc40969166085
VladimirJ123 <jovanovic.ra128.2018@uns.ac.rs>
2021-11-17 18:49:50 +0100

    feat(d02): do repository pattern for pharmacy

---
+++ Pharmacy API/Pharmacy/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Pharmacy.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+    }
+}
---
+++ Pharmacy API/Pharmacy/Service/MedicineService.cs
@@ -0,0 +1,38 @@
+﻿using Pharmacy.Model;
+using Pharmacy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Service
+{
+    class MedicineService : IService<Medicine>
+    {
+        private MedicineRepository _medicineRepository;
+
+        public MedicineService(MedicineRepository medicineRepository)
+        {
+            this._medicineRepository = medicineRepository;
+        }
+
+        public void Add(Medicine entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Medicine> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine GetOneById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Medicine entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
