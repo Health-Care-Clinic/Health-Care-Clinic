58bf092791cbfccc721906ffa657c7126d59e158
marija.petrovic <marijap@protonmail.com>
2021-11-17 23:06:06 +0100

    feat(12): ssh file transfer protocol

---
+++ HealthCareClinic/Integration API/Controller/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Integration_API.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integration_API.Controller
+{
+    [Route("hospital/[controller]")]
+    public class ReportController : ControllerBase
+    {
+        private readonly ReportService reportService;
+
+        public ReportController()
+        {
+            this.reportService = new ReportService();
+        }
+
+        [HttpPost("ftp/{fileName}")]
+        public IActionResult UploadFile(String fileName)
+        {
+            reportService.UploadFile(fileName);
+            return Ok();
+        }
+
+        [HttpGet("ftp/{fileName}")]
+        public IActionResult DownloadFile(String fileName)
+        {
+            reportService.DownloadFile(fileName);
+            return Ok();
+        }
+    }
+}
---
+++ HealthCareClinic/Integration API/Service/ReportService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Integration_API.DTO;
+using Renci.SshNet;
+
+namespace Integration_API.Service
+{
+    public class ReportService
+    {
+        public void UploadFile(String fileName)
+        {
+            var credentials = ServerCredentialsDTO.GetInstance();
+            using SftpClient client =
+                new SftpClient(new PasswordConnectionInfo(credentials.Ip, credentials.Username, credentials.Password));
+            client.Connect();
+            String sourceFile = ServerCredentialsDTO.GetInstance().SourceFolder + Path.DirectorySeparatorChar +
+                                fileName + ".txt";
+
+            using (Stream stream = File.OpenRead(sourceFile))
+            {
+                client.UploadFile(stream, Path.GetFileName(sourceFile), x => { Console.WriteLine(x); });
+            }
+
+            client.Disconnect();
+        }
+
+        public void DownloadFile(String fileName)
+        {
+            var credentials = ServerCredentialsDTO.GetInstance();
+            using SftpClient client =
+                new SftpClient(new PasswordConnectionInfo(credentials.Ip, credentials.Username, credentials.Password));
+            client.Connect();
+            String sourceFile = ServerCredentialsDTO.GetInstance().ServerFolder + Path.DirectorySeparatorChar + fileName;
+
+            using (Stream fileStream = File.OpenWrite("MedSpecifications" + Path.DirectorySeparatorChar + fileName + ".txt"))
+            {
+                client.DownloadFile(fileName + ".txt", fileStream);
+            }
+            client.Disconnect();
+        }
+    }
+}
