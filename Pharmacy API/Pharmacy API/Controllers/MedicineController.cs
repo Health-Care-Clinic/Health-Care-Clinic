85562d567ec5086462b23b152b21c02b9a7e113d
ra74-2018 <nikolajovisic4@gmail.com>
2021-11-17 17:12:38 +0100

    feat(i13): create medicine controller

---
+++ Pharmacy API/Pharmacy API/Controllers/MedicineController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicineController : ControllerBase
+    {
+        [HttpPost("medicineExistsInQuantity")]
+        public IActionResult MedicineExistsInQuantity(int medicineId, int quantity)
+        {
+            //call to service
+            return Ok(true);
+        }
+    }
+}
