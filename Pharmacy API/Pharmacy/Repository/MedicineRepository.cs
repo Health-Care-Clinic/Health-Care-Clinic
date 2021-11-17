0398b46bb4f0795d473451d0f17144af339ab508
VladimirJ123 <73643698+VladimirJ123@users.noreply.github.com>
2021-11-17 20:56:04 +0100

    feat(d02)/develop drug procurement system (#31
    * feat(d02): do repository pattern for pharmacy
    
    * feat(d02): do CRUD operations for medicine

---
+++ Pharmacy API/Pharmacy/Repository/MedicineRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Repository
+{
+    public class MedicineRepository : Repository<Medicine>, IMedicineRepository
+    {
+        public MedicineRepository(PharmacyDbContext context) : base(context)
+        {
+        }
+
+        public PharmacyDbContext PharmacyDbContext
+        {
+            get { return Context as PharmacyDbContext; }
+        }
+    }
+}
---
+++ Pharmacy API/Pharmacy/Service/MedicineService.cs
@@ -0,0 +1,42 @@
+﻿using Pharmacy.Model;
+using Pharmacy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pharmacy.Service
+{
+    class MedicineService : IService<Medicine>
+    {
+        private IMedicineRepository _medicineRepository;
+      
+
+
+        public MedicineService(MedicineRepository medicineRepository)
+        {
+            this._medicineRepository = medicineRepository;
+        }
+
+        public void Add(Medicine entity)
+        {
+            this._medicineRepository.Add(entity);
+            this._medicineRepository.Save();
+        }
+
+        public IEnumerable<Medicine> GetAll()
+        {
+            return this._medicineRepository.GetAll();
+        }
+
+        public Medicine GetOneById(int id)
+        {
+            return this._medicineRepository.GetById(id);
+        }
+
+        public void Remove(Medicine entity)
+        {
+            this._medicineRepository.Remove(entity);
+            this._medicineRepository.Save();
+        }
+    }
+}
