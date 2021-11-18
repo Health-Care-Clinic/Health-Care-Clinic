c1817b79eed1432f531e8b7709c75f5567b55d74
boskoo <andrija.ancobosnjakovic@gmail.com>
2021-11-18 10:01:28 +0100

    feat(p121): initial setup

---
+++ HealthCareClinic/Hospital/Repository/Interface/IPatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using ClinicCore.Storages;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Repository.Interface
+{
+    public interface IPatientRepository : IRepository<Patient>
+    {
+    }
+}
