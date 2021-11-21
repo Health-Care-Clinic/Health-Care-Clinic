c91957c7497a2d0476e0e7b15ce32d31dafc323e
miodragprodanovic <73445973+miodragprodanovic@users.noreply.github.com>
2021-11-21 16:58:05 +0100

    feat(e13): choose room from which equipment is moved (#46)

--- ManagerApplication/ManagerApp/src/app/component/moving-equipment/equipment-list.component.ts
+++ ManagerApplication/ManagerApp/src/app/component/moving-equipment/equipment-list.component.ts
@@ -11,6 +11,8 @@ export class EquipmentListComponent implements OnInit {
   allEquipment: any;
   equipmentNames: any;
   selectedEquipment: any;
+  roomsWithEquipment: any;
+  selectedRoom: any;
 
   constructor(private hospitalMapService: HospitalMapService) {
     this.hospitalMapService.getAllEquipment().subscribe(equipmentFromBack =>{
@@ -24,14 +26,60 @@ export class EquipmentListComponent implements OnInit {
     })
    }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    let next2 = document.getElementById('next2') as HTMLButtonElement;
+    next2.disabled = true;
+    let back1 = document.getElementById('back1') as HTMLButtonElement;
+    back1.disabled = true;
+    let select2 = document.getElementById('select2') as HTMLSelectElement;
+    select2.disabled = true;
+   }
 
   onSubmit() : void{
-    let next1 = document.getElementById('next1');
-    next1.setAttribute('disabled', 'true');
+    let next1 = document.getElementById('next1') as HTMLButtonElement;
+    next1.disabled = true;
     let select1 = document.getElementById('select1') as HTMLSelectElement;
-    select1.setAttribute('disabled', 'true');
+    select1.disabled = true;
+    
+    let next2 = document.getElementById('next2') as HTMLButtonElement;
+    next2.disabled = false;
+    let back1 = document.getElementById('back1') as HTMLButtonElement;
+    back1.disabled = false;
+    let select2 = document.getElementById('select2') as HTMLSelectElement;
+    select2.disabled = false;
+
     this.selectedEquipment = select1.value;
+
+    let roomIds = []
+    this.roomsWithEquipment = []
+    for (let e of this.allEquipment) {
+      if (e.name === this.selectedEquipment){
+        roomIds.push(e.roomId)
+        this.roomsWithEquipment.push(e)
+      }
+    }
+
+    this.hospitalMapService.getRooms().subscribe(roomsFromBack =>{
+      let allRooms = roomsFromBack;
+    })
+  }
+
+  onSubmit2() : void {
+    let next2 = document.getElementById('next2') as HTMLButtonElement;
+    next2.disabled = true;
+    let back1 = document.getElementById('back1') as HTMLButtonElement;
+    back1.disabled = true;
+    let select2 = document.getElementById('select2') as HTMLSelectElement;
+    select2.disabled = true;
+
+    this.selectedRoom = select2.value;
+  }
+
+  back1() : void {
+    let next1 = document.getElementById('next1') as HTMLButtonElement;
+    next1.disabled = false;
+    let select1 = document.getElementById('select1') as HTMLSelectElement;
+    select1.disabled = false;
   }
 
 }
