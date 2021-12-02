fbca9ea423279b90738128de314839c15b52c3b3
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2021-12-02 14:30:17 +0100

    feat(i22): implement compression of old pharmacy files

---
+++ Pharmacy API/Pharmacy/Service/FileCompressionService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Ionic.Zip;
+
+namespace Pharmacy.Service
+{
+    public class FileCompressionService
+    {
+        public static void CompressFiles()
+        {
+            string path = "..\\..\\Pharmacy API\\Pharmacy API\\MedSpecifications\\";
+
+            while(true)
+            {
+                DirectoryInfo d = new DirectoryInfo(@path);
+
+                FileInfo[] files = d.GetFiles();
+                List<FileInfo> filesToCompress = new List<FileInfo>();
+
+                GetFilesToCompress(files, filesToCompress);
+
+                AddFilesToArchive(path, filesToCompress);
+
+                RemoveCompressedFiles(filesToCompress);
+
+                Thread.Sleep(10000);
+            }
+        }
+
+        private static void RemoveCompressedFiles(List<FileInfo> filesToCompress)
+        {
+            foreach (FileInfo fi in filesToCompress)
+            {
+                File.Delete(fi.FullName);
+            }
+        }
+
+        private static void AddFilesToArchive(string path, List<FileInfo> filesToCompress)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                if (filesToCompress.Count == 0)
+                {
+                    return;
+                }
+
+                foreach (FileInfo fi in filesToCompress)
+                {
+                    zip.AddFile(fi.FullName, "Old data");
+                }
+
+                string zipPath = path + DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day + "-" +
+                    DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second + "-" + DateTime.Now.Millisecond + ".zip";
+                zip.Save(zipPath);
+            }
+        }
+
+        private static void GetFilesToCompress(FileInfo[] files, List<FileInfo> filesToCompress)
+        {
+            foreach (FileInfo file in files)
+            {
+                if (/*file.CreationTime < DateTime.Now.AddDays(-7) && */file.Extension != ".zip")
+                {
+                    filesToCompress.Add(file);
+                }
+            }
+        }
+    }
+}
