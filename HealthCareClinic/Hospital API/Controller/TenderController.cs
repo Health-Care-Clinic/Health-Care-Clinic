59ad4cace3f924e54056cfc7d7c4850f588a720a
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2021-12-23 03:43:21 +0100

    feat(i10): send tender via rabbitMQ

---
+++ HealthCareClinic/Hospital API/Controller/TenderController.cs
@@ -0,0 +1,44 @@
+﻿using Hospital.Tendering.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenderController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult ReceiveFeedback(Tender tender)
+        {
+            tender = new Tender(null, new Price(500), new DateRange(DateTime.Now, DateTime.Now.AddDays(7)), "Test slanja tendera.");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "tender",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(tender));
+
+                channel.BasicPublish(exchange: "",
+                                        routingKey: "tender",
+                                        basicProperties: null,
+                                        body: body);
+                Console.WriteLine(" [x] Sent {0}", tender.TenderResponseDescription);
+            }
+            return Ok("success");
+        }
+    }
+}
---
+++ Pharmacy API/Pharmacy/Tendering/Service/RabbitMQTenderService.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Pharmacy.ApiKeys.Model;
+using Pharmacy.Interfaces.Repository;
+using Pharmacy.Tendering.Model;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Tendering.Service
+{
+    public class RabbitMQTenderService : BackgroundService
+    {
+        IConnection connection;
+        IModel channel;
+        public IServiceProvider Services { get; }
+
+        public RabbitMQTenderService(IServiceProvider services)
+        {
+            Services = services;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "tender",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                var jsonMessage = Encoding.UTF8.GetString(body);
+                Tender tender;
+                tender = JsonConvert.DeserializeObject<Tender>(jsonMessage);
+
+                Console.WriteLine(" [x] Received {0}", tender.TenderResponseDescription);
+                /*using (var scope = Services.CreateScope())
+                {
+                    var pharmacyPromotionRepository =
+                        scope.ServiceProvider
+                            .GetRequiredService<IPharmacyPromotionRepository>();
+
+                    pharmacyPromotionRepository.Add(pharmacyPromotion);
+                    pharmacyPromotionRepository.Save();
+                }*/
+            };
+            channel.BasicConsume(queue: "tender",
+                                    autoAck: true,
+                                    consumer: consumer);
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            channel.Close();
+            connection.Close();
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
