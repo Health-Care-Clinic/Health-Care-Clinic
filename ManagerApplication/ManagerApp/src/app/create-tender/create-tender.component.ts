c734b88e24df165c1a026cb16b4201f096b91e71
anagrahovac <grahovac.ana99@gmail.com>
2021-12-23 12:16:10 +0100

    feat(i10): add view for creating tender

--- ManagerApplication/ManagerApp/src/app/create-tender/create-tender.component.ts
+++ ManagerApplication/ManagerApp/src/app/create-tender/create-tender.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Medicine } from '../model/medicine';
 
 @Component({
   selector: 'app-create-tender',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTenderComponent implements OnInit {
 
-  constructor() { }
+  public medicines: Medicine[] = [];
+  public newMedicine: string = "";
+  public newQuantity: number = null;
+
+  constructor() { 
+    //this.medicines = [{"name": "brufen", "quantity": 500}, {"name": "aspirin", "quantity": 800}];
+  }
 
   ngOnInit(): void {
+  }
+
+  addMedicine(): void {
+    if (this.newMedicine != "" && this.newQuantity > 0) {
+      let medicine = {"name": this.newMedicine, "quantity": this.newQuantity}
+      this.medicines.push(medicine);
+      this.newMedicine = "";
+      this.newQuantity = null;
+    }
   }
 
 }
