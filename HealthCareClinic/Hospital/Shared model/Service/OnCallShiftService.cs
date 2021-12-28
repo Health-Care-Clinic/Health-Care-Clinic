624ac8af561a2a8fc90e45f082d9230f3a6602d2
vrba3 <74838282+vrba3@users.noreply.github.com>
2021-12-28 14:29:18 +0100

    feat(e46): resolving code smells

--- HealthCareClinic/Hospital/Shared model/Service/OnCallShiftService.cs
+++ HealthCareClinic/Hospital/Shared model/Service/OnCallShiftService.cs
@@ -53,21 +53,16 @@ namespace Hospital.Shared_model.Service
         public List<DateTime> GetFreeDates(int month)
         {
             List<DateTime> freeDates = returnDates(month);
-            IEnumerable<OnCallShift> onCallShifts = _onCallShiftRepository.GetAll();
-            foreach (OnCallShift ocshift in onCallShifts)
-            {
-                if (!isDateFree(ocshift.Date, freeDates))
-                    freeDates.Remove(new DateTime(ocshift.Date.Year, ocshift.Date.Month, ocshift.Date.Day));
-            }
 
             return freeDates;
         }
 
-        private Boolean isDateFree(DateTime busyDate, List<DateTime> freeDates)
+        private Boolean isDateFree(DateTime date)
         {
-            foreach (DateTime d in freeDates)
+            IEnumerable<OnCallShift> onCallShifts = _onCallShiftRepository.GetAll();
+            foreach (OnCallShift shift in onCallShifts)
             {
-                if (d.Year == busyDate.Year && d.Month == busyDate.Month && d.Day == busyDate.Day)
+                if (shift.Date.Year == date.Year && shift.Date.Month == date.Month && shift.Date.Day == date.Day)
                     return false;
             }
 
@@ -80,30 +75,32 @@ namespace Hospital.Shared_model.Service
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
                 for (int i = 1; i <= 31; i++)
                 {
-                    dates.Add(new DateTime(DateTime.Now.Year, month, i));
+                    DateTime checkDate = new DateTime(DateTime.Now.Year, month, i);
+                    if (isDateFree(checkDate))
+                        dates.Add(checkDate);
                 }
             else if (month == 4 || month == 6 || month == 9 || month == 11)
-                for (int i = 1; i <= 30; i++)
+                for (int i = 1; i <= 30; i++) 
                 {
-                    dates.Add(new DateTime(DateTime.Now.Year, month, i));
+                    DateTime checkDate = new DateTime(DateTime.Now.Year, month, i);
+                    if (isDateFree(checkDate))
+                        dates.Add(checkDate);
+                }         
+            else
+                if (DateTime.Now.Year % 4 == 0)
+                for (int i = 1; i <= 29; i++)
+                {
+                    DateTime checkDate = new DateTime(DateTime.Now.Year, month, i);
+                    if (isDateFree(checkDate))
+                        dates.Add(checkDate);
                 }
             else
-            {
-                if (DateTime.Now.Year % 4 == 0)
+                for (int i = 1; i <= 28; i++)
                 {
-                    for (int i = 1; i <= 29; i++)
-                    {
-                        dates.Add(new DateTime(DateTime.Now.Year, month, i));
-                    }
+                    DateTime checkDate = new DateTime(DateTime.Now.Year, month, i);
+                    if (isDateFree(checkDate))
+                        dates.Add(checkDate);
                 }
-                else
-                {
-                    for (int i = 1; i <= 28; i++)
-                    {
-                        dates.Add(new DateTime(DateTime.Now.Year, month, i));
-                    }
-                }
-            }
 
             return dates;
         }
