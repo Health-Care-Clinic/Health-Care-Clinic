87239999528ccb1cb440f9c8b762d4cc87d7aee1
Aleksandar Savic <savic.ra8.2018@uns.ac.rs>
2021-12-28 02:32:48 +0100

    feat(e37): show hospital shifts

---
+++ HealthCareClinic/Hospital API/Controller/WorkDayShiftController.cs
@@ -0,0 +1,31 @@
+﻿using Hospital.Shared_model.Service;
+using Hospital_API.Adapter;
+using Hospital_API.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital_API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkDayShiftController : ControllerBase
+    {
+        private readonly IWorkDayShiftService workDayShiftService;
+        public WorkDayShiftController(IWorkDayShiftService workDayShiftService)
+        {
+            this.workDayShiftService = workDayShiftService;
+        }
+
+        [HttpGet("getWorkDayShifts")]
+        public IActionResult GetWorkDayShifts()
+        {
+            List<WorkDayShiftDTO> allWorkDayShifts = new List<WorkDayShiftDTO>();
+            workDayShiftService.GetAll().ToList().ForEach(WorkDayShift
+                => allWorkDayShifts.Add(WorkDayShiftAdapter.WorkDayShiftToWorkDayShiftDTO(WorkDayShift)));
+            return Ok(allWorkDayShifts);
+        }
+    }
+}
