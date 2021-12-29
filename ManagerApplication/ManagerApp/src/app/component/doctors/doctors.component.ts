9fd747faf73205a199b1027f5ba30eccff64938b
vrba3 <74838282+vrba3@users.noreply.github.com>
2021-12-29 17:56:42 +0100

    feat(e34): showing shifts (#139)

--- ManagerApplication/ManagerApp/src/app/component/doctors/doctors.component.ts
+++ ManagerApplication/ManagerApp/src/app/component/doctors/doctors.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IDoctor } from 'src/app/model/patient/doctor';
 import { DoctorsService } from 'src/app/services/doctors.service';
+import { ShiftsService } from 'src/app/services/shifts.service';
+import { Shift } from 'src/app/model/shift';
 
 @Component({
   selector: 'app-doctors',
@@ -10,14 +12,34 @@ import { DoctorsService } from 'src/app/services/doctors.service';
 })
 export class DoctorsComponent implements OnInit {
 
-  constructor(private doctorService: DoctorsService,private _route: ActivatedRoute,private router: Router) { }
+  constructor(private doctorService: DoctorsService,private _route: ActivatedRoute,private router: Router, private shiftService: ShiftsService) { }
 
   doctors: Array<IDoctor>
+  shifts: Array<Shift>
 
   ngOnInit(): void {
     this.doctorService.getAllDoctors().subscribe(ret =>{
       this.doctors = ret;
     })
+    this.shiftService.getAllShifts().subscribe(ret => {
+      this.shifts = ret;
+      
+      
+    });
   }
+
+  returnShift(idShift: number): string {
+    for(let shift of this.shifts) {
+      if(shift.id == idShift) {
+        let sTime = shift.startTime.toString().split('T')[1];
+        let eTime = shift.endTime.toString().split('T')[1];
+        return (sTime + ' - ' + eTime);
+      }
+        
+    }
+      return ''
+  }
+
+
 
 }
