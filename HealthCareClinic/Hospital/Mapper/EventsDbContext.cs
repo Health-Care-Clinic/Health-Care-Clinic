be12cb7be990f33d1db3b9f3edf6ca64401ffcc8
ABjelobaba <bjelobaba.ra136.2018@uns.ac.rs>
2022-01-08 02:56:49 +0100

    feat(d14): implement event storing

---
+++ HealthCareClinic/Hospital/Mapper/EventsDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Hospital.Events.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Mapper
+{
+    public class EventsDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public EventsDbContext() { }
+
+        public EventsDbContext(DbContextOptions<EventsDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Event>().HasData(
+                new Event { Id = 1, Timestamp = new DateTime(2022, 01, 03, 12, 35, 42), Content = "", UserId = 1 },
+                new Event { Id = 2, Timestamp = new DateTime(2022, 01, 03, 13, 32, 17), Content = "", UserId = 1 },
+                new Event { Id = 3, Timestamp = new DateTime(2022, 01, 04, 18, 21, 8), Content = "", UserId = 2 },
+                new Event { Id = 4, Timestamp = new DateTime(2022, 01, 05, 9, 41, 28), Content = "", UserId = 3 }
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(CreateConnectionStringFromEnvironment()).UseLazyLoadingProxies();
+        }
+
+        private static string CreateConnectionStringFromEnvironment()
+        {
+            var server = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
+            var port = Environment.GetEnvironmentVariable("DATABASE_PORT") ?? "5432";
+            var database = Environment.GetEnvironmentVariable("DATABASE_SCHEMA") ?? "eventsDb";
+            var user = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "postgres";
+            var password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "password";
+
+            return
+                $"Server={server};Port={port};Database={database};User ID={user};Password={password};";
+        }
+    }
+}
