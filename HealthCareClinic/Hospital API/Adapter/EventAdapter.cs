1bda8a20ab6cc51ebdf1b84ef9f971c95030848a
Aleksandar Bjelobaba <73204377+ABjelobaba@users.noreply.github.com>
2022-01-08 22:25:41 +0100

    feat(d14): implement event storing (#153)
    * feat(d14): implement event storing
    
    * style: rename parameter

---
+++ HealthCareClinic/Hospital API/Adapter/EventAdapter.cs
@@ -0,0 +1,42 @@
+﻿using Hospital.Events.Model;
+using Hospital_API.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital_API.Adapter
+{
+    public class EventAdapter
+    {
+        public static Event EventDTOToEvent(EventDTO dto)
+        {
+            Event ev = new Event();
+            ev.Id = dto.Id;
+            ev.Timestamp = dto.Timestamp;
+            ev.Content = dto.Content;
+            ev.UserId = dto.UserId;
+            return ev;
+        }
+        public static EventDTO EventToEventDTO(Event ev)
+        {
+            EventDTO dto = new EventDTO();
+            dto.Id = ev.Id;
+            dto.Timestamp = ev.Timestamp;
+            dto.Content = ev.Content;
+            dto.UserId = ev.UserId;
+            return dto;
+        }
+
+        public static List<EventDTO> EventsToEventDTOs(List<Event> events)
+        {
+            List<EventDTO> eventDTOs = new List<EventDTO>();
+            foreach (Event ev in events)
+            {
+                EventDTO dto = EventToEventDTO(ev);
+                eventDTOs.Add(dto);
+            }
+            return eventDTOs;
+        }
+    }
+}
---
+++ HealthCareClinic/Hospital API/Controller/EventController.cs
@@ -0,0 +1,41 @@
+﻿using Hospital.Events.Model;
+using Hospital.Events.Service;
+using Hospital_API.Adapter;
+using Hospital_API.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital_API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet("getAllEvents")]
+        public IActionResult GetAllEvents()
+        {
+            List<EventDTO> eventDTOs = EventAdapter.EventsToEventDTOs((List<Event>) _eventService.GetAll());
+            return Ok(eventDTOs);
+        }
+
+        [HttpPost("createEvent")]
+        public IActionResult CreateEvent(EventDTO eventDto)
+        {
+            Event ev = EventAdapter.EventDTOToEvent(eventDto);
+
+            _eventService.Add(ev);
+
+            return Ok();
+        }
+    }
+}
