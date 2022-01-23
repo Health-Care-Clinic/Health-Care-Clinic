0fa72240a6e4e1fc007c758b272bebd82f3ed051
ra74-2018 <nikolajovisic4@gmail.com>
2022-01-23 18:35:23 +0100

    test: init urgent procurement test files

---
+++ HealthCareClinic/HospitalSeleniumTests/Pages/UrgentProcurementPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HospitalSeleniumTests.Pages
+{
+    public class UrgentProcurementPage
+    {
+        private readonly IWebDriver _driver;
+        public const string URI = "http://localhost:4200/urgent-procurement";
+        private IWebElement MedicineName => _driver.FindElement(By.Id("medicineName"));
+        private IWebElement MedicineAmount => _driver.FindElement(By.Id("medicineAmount"));
+        public string Title => _driver.Title;
+
+        public UrgentProcurementPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public bool MedicineNameDisplayed()
+        {
+            return MedicineName.Displayed;
+        }
+
+        public bool MedicineAmountDisplayed()
+        {
+            return MedicineAmount.Displayed;
+        }
+        
+
+        public void Wait()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 20));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.UrlToBe(HomePage.URI));
+        }
+
+        public void Navigate() => _driver.Navigate().GoToUrl(URI);
+    }
+}
+
---
+++ HealthCareClinic/HospitalSeleniumTests/UrgentProcurementTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace HospitalSeleniumTests
+{
+    public class UrgentProcurementTests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        private Pages.UrgentProcurementPage urgentProcurementPage;
+        private Pages.LoginPage loginPage;
+
+        public UrgentProcurementTests()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+
+            _driver = new ChromeDriver(options);
+
+            urgentProcurementPage = new Pages.UrgentProcurementPage(_driver);
+            urgentProcurementPage.Navigate();
+            
+
+            Assert.Equal(_driver.Url, Pages.CreateTenderPage.URI);
+
+            Assert.True(urgentProcurementPage.MedicineNameDisplayed());
+            Assert.True(urgentProcurementPage.MedicineAmountDisplayed());
+        }
+
+        [Fact]
+        public void SuccessfulSubmit()
+        {
+            Assert.True(urgentProcurementPage.MedicineNameDisplayed());
+            Assert.True(urgentProcurementPage.MedicineAmountDisplayed());
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
+
