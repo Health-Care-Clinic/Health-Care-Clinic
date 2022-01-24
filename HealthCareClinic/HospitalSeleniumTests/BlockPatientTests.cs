7ab29b2f748e420086a9fa71b5380910a0952ce6
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2022-01-24 15:59:34 +0100

    fix: reorganize options

--- HealthCareClinic/HospitalSeleniumTests/BlockPatientTests.cs
+++ HealthCareClinic/HospitalSeleniumTests/BlockPatientTests.cs
@@ -19,12 +19,12 @@ namespace HospitalSeleniumTests
         {
             // options for launching Google Chrome
             ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--no-sandbox");               // Bypass OS security model
+            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
             options.AddArguments("start-maximized");            // open Browser in maximized mode
             options.AddArguments("disable-infobars");           // disabling infobars
             options.AddArguments("--disable-extensions");       // disabling extensions
             options.AddArguments("--disable-gpu");              // applicable to windows os only
-            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
-            options.AddArguments("--no-sandbox");               // Bypass OS security model
             options.AddArguments("--disable-notifications");    // disable notifications
 
 #if RELEASE
