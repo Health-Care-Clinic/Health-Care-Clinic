14529d91e8662017aa09b694824e996c40f141df
miodragprodanovic <73445973+miodragprodanovic@users.noreply.github.com>
2022-01-24 23:02:04 +0100

    feat(e51)/doctor schedule aggregate (#183)
    * feat(e49): refactoring
    
    * improvement(e49): refactoring
    
    * improvement(e49): add validation to entity
    
    * refactoring(e49): refactoring
    
    * refactoring(e49): remove code smells
    
    * refactoring(e49): removing code smells
    
    * refactoring(e49): remove code smells
    
    * refactoring(e49): removing code smells
    
    * refactoring(e49): refactoring
    
    * feat(e52): Aggregate pattern on class Transfer
    
    * refactoring(e52): removing code smells
    
    * refactoring(e52): remove code smells
    
    * refactoring(e52): removing code smells
    
    * feat(e51): Aggregate pattern on class DoctorSchedule
    
    * refactoring(e51): removing bugs
    
    Co-authored-by: vrba3 <74838282+vrba3@users.noreply.github.com>
    Co-authored-by: MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>

---
+++ HealthCareClinic/Hospital/Shared model/Service/IDoctorScheduleService.cs
@@ -0,0 +1,31 @@
+﻿using Hospital.Shared_model.Interface;
+using Hospital.Shared_model.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Shared_model.Service
+{
+    public interface IDoctorScheduleService : IService<DoctorSchedule>
+    {
+        public bool CheckVacationAvailability(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, Vacation vacation);
+
+        public bool AddVacation(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, Vacation vacation);
+
+        public bool RemoveVacation(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, Vacation vacation);
+
+        public bool ChangeWorkDayShift(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, WorkDayShift newWorkDayShift);
+
+        public bool CheckOnCallShiftAvailability(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, OnCallShift onCallShift);
+
+        public bool AddOnCallShift(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, OnCallShift onCallShift);
+
+        public bool RemoveOnCallShift(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, OnCallShift onCallShift);
+
+        public bool CheckAppointmentAvailability(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, Appointment appointment);
+
+        public bool AddAppointment(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, Appointment appointment);
+
+        public bool RemoveAppointment(Doctor doctor, WorkDayShift workDayShift, List<Vacation> vacations, List<OnCallShift> onCallShifts, List<Appointment> appointments, Appointment appointment);
+    }
+}
