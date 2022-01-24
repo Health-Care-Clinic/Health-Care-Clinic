f9112ca167a9a63756f441438c0e000705831f5e
Lea Kalmar <kalmar.ra130.2018@uns.ac.rs>
2022-01-24 15:45:45 +0100

    fix: csproj

--- HealthCareClinic/HospitalSeleniumTests/BlockPatientTests.cs
+++ HealthCareClinic/HospitalSeleniumTests/BlockPatientTests.cs
@@ -1,8 +1,8 @@
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
-using NUnit.Framework;
 
 namespace HospitalSeleniumTests
 {
