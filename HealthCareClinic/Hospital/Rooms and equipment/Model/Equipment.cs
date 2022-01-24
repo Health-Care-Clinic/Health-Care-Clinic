e7dd905b14ef1a1411ae8a36c7afe2cc5781e5d7
miodragprodanovic <73445973+miodragprodanovic@users.noreply.github.com>
2022-01-24 21:16:41 +0100

    feat(e52)/moving equipment aggregate (#180)
    * feat(e49): refactoring
    
    * improvement(e49): refactoring
    
    * improvement(e49): add validation to entity
    
    * refactoring(e49): refactoring
    
    * refactoring(e49): remove code smells
    
    * refactoring(e49): removing code smells
    
    * refactoring(e49): remove code smells
    
    * refactoring(e49): removing code smells
    
    * refactoring(e49): refactoring
    
    * feat(e52): Aggregate pattern on class Transfer
    
    * refactoring(e52): removing code smells
    
    * refactoring(e52): remove code smells
    
    * refactoring(e52): removing code smells
    
    Co-authored-by: vrba3 <74838282+vrba3@users.noreply.github.com>
    Co-authored-by: MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>

--- HealthCareClinic/Hospital/Rooms and equipment/Model/Equipment.cs
+++ HealthCareClinic/Hospital/Rooms and equipment/Model/Equipment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Hospital.Rooms_and_equipment.Model
@@ -24,8 +25,37 @@ namespace Hospital.Rooms_and_equipment.Model
             Type = type;
             Quantity = quantity;
             RoomId = roomId;
+            Validate();
+        }
+
+        public Equipment(string name,  int quantity)
+        {
+            Id = 0;
+            Name = name;
+            Type = EquipmentType.Dynamic;
+            Quantity = quantity;
+            RoomId = 0;
+            Validate();
         }
         public Equipment() { }
+
+        private void Validate()
+        {
+            CheckName();
+            CheckQuantity();
+        }
+
+        private void CheckName()
+        {
+            if (string.IsNullOrEmpty(Name))
+                throw new ArgumentException("Equipment name can't be empty!");
+        }
+
+        private void CheckQuantity()
+        {
+            if (Quantity < 0)
+                throw new ArgumentException("Equipment quantity can't be less than zero!");
+        }
 
         public override bool Equals(object obj)
         {
@@ -40,6 +70,27 @@ namespace Hospital.Rooms_and_equipment.Model
         public override int GetHashCode()
         {
             return HashCode.Combine(Id, Name, Type, Quantity, RoomId);
+        }
+
+        public bool EquipmentExistsInRoom(List<Equipment> roomEquipment)
+        {
+            if (roomEquipment.Select(eq => eq.Name).Any(x => x.Equals(Name)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetQuantityOfEquipmentInOtherRoom(List<Equipment> roomEquipment)
+        {
+            int quantity = 0;
+            foreach (Equipment eq in roomEquipment.Where(x => x.Name.Equals(Name)))
+            {
+                quantity += eq.Quantity;
+            }
+
+            return quantity;
         }
     }
 }
--- HealthCareClinic/Hospital/Rooms and equipment/Model/Transfer.cs
+++ HealthCareClinic/Hospital/Rooms and equipment/Model/Transfer.cs
@@ -6,51 +6,148 @@ namespace Hospital.Rooms_and_equipment.Model
 {
     public class Transfer
     {
-        public int Id 
-        {
-            get; set;
-        }
-        public String Equipment 
-        {
-            get; set;
-        }
-
-        public int Quantity
-        {
-            get; set;
-        }
-
-        public int SourceRoomId
-        {
-            get; set;
-        }
-
-        public int DestinationRoomId
-        {
-            get; set;
-        }
-
-        public DateTime Date
-        {
-            get; set;
-        }
-
-        public int Duration
-        {
-            get; set;
-        }
-
-        public Transfer(int id, String equipment, int quantity, int sourceID, int destinationID, DateTime date, int duration) {
-            Id = id;
-            Equipment = equipment;
-            Quantity = quantity;
-            SourceRoomId = sourceID;
-            DestinationRoomId = destinationID;
-            Date = date;
-            Duration = duration;
-        }
+        public int Id { get; set; }
+        public Equipment Equipment { get; set; }
+        public RoomsForTransfer RoomsForTransfer { get; set; }
+        public DateAndDuration DateAndDuration { get; set; }
 
         public Transfer() { }
+        public Transfer(int id, Equipment equipment, int sourceID, int destinationID, DateTime date, int duration) {
+            Id = id;
+            Equipment = equipment;
+            RoomsForTransfer = new RoomsForTransfer(sourceID, destinationID);
+            DateAndDuration = new DateAndDuration(date, duration);
+        }
 
+        public List<Transfer> GetTransfersOfSourceAndDestination(List<Transfer> existingTransfers)
+        {
+            List<Transfer> allTransfersOfSourceAndDestinationRoom = new List<Transfer>();
+            foreach (Transfer t in existingTransfers)
+            {
+                if (t.RoomsForTransfer.SourceRoomId == RoomsForTransfer.SourceRoomId 
+                    || t.RoomsForTransfer.DestinationRoomId == RoomsForTransfer.DestinationRoomId 
+                    || t.RoomsForTransfer.SourceRoomId == RoomsForTransfer.DestinationRoomId 
+                    || t.RoomsForTransfer.DestinationRoomId == RoomsForTransfer.SourceRoomId)
+                {
+                    allTransfersOfSourceAndDestinationRoom.Add(t);
+                }
+            }
+
+            return allTransfersOfSourceAndDestinationRoom;
+        }
+
+        public DateTime RoundMinutes(DateTime now)
+        {
+            if (now.Minute < 15)
+            {
+                return ChangeTime(now.Year, now.Month, now.Day, now.Hour, 15, 0);
+            }
+            else if (now.Minute < 30)
+            {
+                return ChangeTime(now.Year, now.Month, now.Day, now.Hour, 30, 0);
+            }
+            else if (now.Minute < 45)
+            {
+                return ChangeTime(now.Year, now.Month, now.Day, now.Hour, 45, 0);
+            }
+            else if (now.Minute < 60)
+            {
+                return ChangeTime(now.Year, now.Month, now.Day, now.Hour + 1, 0, 0);
+            }
+
+            return now;
+        }
+
+        public DateTime AddDuration(DateTime transferAndDuration)
+        {
+            if ((transferAndDuration.Minute + (DateAndDuration.Duration % 60)) < 60)
+            {
+                return transferAndDuration.AddMinutes(DateAndDuration.Duration % 60);
+            }
+            else
+            {
+                DateTime now = transferAndDuration.AddHours(1);
+                return ChangeTime(now.Year, now.Month, now.Day, now.Hour, (now.Minute + (DateAndDuration.Duration % 60)) - 60, 0);
+            }            
+        }
+
+        public DateTime AddTime(DateTime today)
+        {
+            if (today.Minute < 45)
+            {
+                today = today.AddMinutes(15);
+            }
+            else
+            {
+                if (today.Hour < 23)
+                {
+                    today = ChangeHours(today.Year, today.Month, today.Day, today.Hour, 0, 0);
+                }
+                else
+                {
+                    if (today.Day < DateTime.DaysInMonth(today.Year, today.Month))
+                    {
+                        today = ChangeDays(today.Year, today.Month, today.Day, 0, 0, 0);
+                    }
+                    else
+                    {
+                        if (today.Month < 12)
+                        {
+                            today = ChangeMonths(today.Year, today.Month, 1, 0, 0, 0);
+                        }
+                        else
+                        {
+                            today = today.AddYears(1);
+                        }
+                    }
+                }
+            }
+
+            return today;
+        }
+
+        private DateTime ChangeTime(int year, int month, int day, int hours, int minutes, int seconds)
+        {
+            return new DateTime(
+                year,
+                month,
+                day,
+                hours,
+                minutes,
+                seconds);
+        }
+
+        private DateTime ChangeMonths(int year, int month, int day, int hours, int minutes, int seconds)
+        {
+            return new DateTime(
+                year,
+                month,
+                day,
+                hours,
+                minutes,
+                seconds).AddMonths(1);
+        }
+
+        private DateTime ChangeDays(int year, int month, int day, int hours, int minutes, int seconds)
+        {
+            return new DateTime(
+                year,
+                month,
+                day,
+                hours,
+                minutes,
+                seconds).AddDays(1);
+        }
+
+        private DateTime ChangeHours(int year, int month, int day, int hours, int minutes, int seconds)
+        {
+            return new DateTime(
+                year,
+                month,
+                day,
+                hours,
+                minutes,
+                seconds).AddHours(1);
+        }
     }
 }
