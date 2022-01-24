65bd0f824c99e4a9aad1150d3ef53e0565ebf91a
vrba3 <74838282+vrba3@users.noreply.github.com>
2022-01-24 22:58:25 +0100

    feat(e55): added-selenium-test (#181)

---
+++ HealthCareClinic/HospitalSeleniumTests/Pages/HomePageManagerApp.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace HospitalSeleniumTests.Pages
+{
+    public class HomePageManagerApp
+    {
+        private readonly IWebDriver driver;
+        public const string URI = "http://localhost:4200/hospital-map";
+        private IWebElement OurFacilityButton => driver.FindElement(By.Id("facility"));
+        private IWebElement DoctorsButton => driver.FindElement(By.Id("doctors"));
+
+        public HomePageManagerApp(IWebDriver driver) 
+        {
+            this.driver = driver;
+        }
+
+        public bool FacilityButtonDisplayed() {
+            return OurFacilityButton.Displayed;
+        }
+
+        public bool DoctorsButtonDisplayed()
+        {
+            return DoctorsButton.Displayed;
+        }
+
+        public void ClickOnFacility()
+        {
+            OurFacilityButton.Click();
+        }
+
+        public void ClickOnDoctors()
+        {
+            DoctorsButton.Click();
+        }
+
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+    }
+}
