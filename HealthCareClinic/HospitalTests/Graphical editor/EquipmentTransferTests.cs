a63076eba8ab6d2aeee31b3a7d50a553b0f9ce6f
Nenad Becanovic <nenadbecanovic99@gmail.com>
2022-08-16 20:37:08 +0200

    Refactor RenovationTests

---
+++ HealthCareClinic/HospitalTests/Graphical editor/EquipmentTransferTests.cs
@@ -0,0 +1,59 @@
+﻿using Hospital.Rooms_and_equipment.Model;
+using Hospital.Rooms_and_equipment.Repository;
+using Hospital.Rooms_and_equipment.Service;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+using static Hospital.Rooms_and_equipment.Model.Equipment;
+
+namespace HospitalUnitTests.Graphical_editor
+{
+    public class EquipmentTransferTests
+    {
+
+        private static IEquipmentRepository CreateStubRepository()
+        {
+            List<Equipment> equipment = new List<Equipment>();
+            var stubRepository = new Mock<IEquipmentRepository>();
+            var firstRoomId = 1;
+            var secondRoomId = 2;
+
+            equipment.Add(new Equipment { Id = 1, Name = "Blanket", Type = EquipmentType.Dynamic, Quantity = 1, RoomId = firstRoomId });
+
+            stubRepository.Setup(m => m.GetEquipmentByRoomId(firstRoomId)).Returns(equipment);
+            stubRepository.Setup(m => m.GetEquipmentByRoomId(secondRoomId)).Returns(new List<Equipment>());
+
+            return stubRepository.Object;
+        }
+
+        [Fact]
+        public void Check_equipment_transfer_from_one_room_to_another()
+        {
+            var equipmentRepository = CreateStubRepository();
+            EquipmentService equipmentService = new EquipmentService(equipmentRepository);
+            var firstRoomId = 1;
+            var secondRoomId = 2;
+            var equipmentName = "Blanket";
+            var quantity = 1;
+            Equipment eq = new Equipment(1, equipmentName, EquipmentType.Dynamic, quantity, secondRoomId);
+
+            List<Equipment> newEquipment = equipmentService.TransferEquipmentFromOneRoomToAnother(secondRoomId, firstRoomId);
+
+            Assert.Contains(eq, newEquipment);
+        }
+
+        [Fact]
+        public void Check_equipment_quantity_when_transfered_from_one_room_to_another()
+        {
+            var equipmentRepository = CreateStubRepository();
+            EquipmentService equipmentService = new EquipmentService(equipmentRepository);
+            var firstRoomId = 1;
+            var secondRoomId = 2;
+            var equipmentName = "Blanket";
+
+            int quantity = equipmentService.GetQuantityOfEquipmentWhenTransferedFromOneRoomToAnother(equipmentName, secondRoomId, firstRoomId);
+
+            Assert.Equal(1, quantity);
+        }
+    }
+}
--- HealthCareClinic/HospitalTests/Graphical editor/RenovationTests.cs
+++ HealthCareClinic/HospitalTests/Graphical editor/RenovationTests.cs
@@ -1,93 +1,59 @@
-﻿using Hospital.Graphical_editor.Model;
-using Hospital.Mapper;
-using Hospital.Rooms_and_equipment.Model;
+﻿using Hospital.Rooms_and_equipment.Model;
 using Hospital.Rooms_and_equipment.Repository;
 using Hospital.Rooms_and_equipment.Service;
 using Hospital.Shared_model.Repository;
-using Hospital_API.Controller;
-using Hospital_API.DTO;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
-using static Hospital.Rooms_and_equipment.Model.Equipment;
 
 namespace HospitalUnitTests.Graphical_editor
 {
     public class RenovationTests
     {
-        private DbContextOptions<HospitalDbContext> CreateStubRepository()
+
+        private static IRenovationRepository CreateRenovationsStubRepository()
         {
-            var options = new DbContextOptionsBuilder<HospitalDbContext>()
-            .UseInMemoryDatabase(databaseName: "Renovations")
-            .Options;
+            List<Renovation> renovations = new List<Renovation>();
+            var stubRepository = new Mock<IRenovationRepository>();
+            var renovationDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0);
 
-            using (var context = new HospitalDbContext(options))
-            {
-                context.Renovations.Add(new Renovation { Id = 1, FirstRoomId = 1, SecondRoomId = 2, Duration = 2, Date = new DateTime(2022, 02, 02, 14, 00, 00), Type = Renovation.RenovationType.Merge });
-                context.Renovations.Add(new Renovation { Id = 2, FirstRoomId = 1, SecondRoomId = 0, Duration = 3, Date = new DateTime(2022, 02, 05, 15, 00, 00), Type = Renovation.RenovationType.Divide });
-                context.Renovations.Add(new Renovation { Id = 3, FirstRoomId = 47, SecondRoomId = 62, Duration = 2, Date = new DateTime(2022, 02, 20, 14, 00, 00), Type = Renovation.RenovationType.Merge });
-                context.Transfer.Add(new Transfer { Id = 1, Equipment = new EquipmentForTransfer("Bed", 5), RoomsForTransfer = new RoomsForTransfer(1, 2), DateAndDuration = new DateAndDuration(new DateTime(2021, 12, 31, 9, 0, 0), 60) });
-                context.Transfer.Add(new Transfer { Id = 2, Equipment = new EquipmentForTransfer("TV", 2), RoomsForTransfer = new RoomsForTransfer(1, 2), DateAndDuration = new DateAndDuration(new DateTime(2021, 11, 28, 11, 0, 0), 30) });
-                context.Transfer.Add(new Transfer { Id = 3, Equipment = new EquipmentForTransfer("Blanket", 7), RoomsForTransfer = new RoomsForTransfer(1, 2), DateAndDuration = new DateAndDuration(new DateTime(2021, 12, 25, 15, 30, 0), 45) });
-                context.Transfer.Add(new Transfer { Id = 4, Equipment = new EquipmentForTransfer("Needle", 25), RoomsForTransfer = new RoomsForTransfer(1, 2), DateAndDuration = new DateAndDuration(new DateTime(2021, 10, 25, 15, 30, 0), 45) });
-                context.Rooms.Add(new Room { Id = 1, Name = "Operation room 1", Type = Room.RoomType.OperationRoom, PositionAndDimension = new PositionAndDimension(10, 100, 100, 200), FloorId = 1 });
-                context.Rooms.Add(new Room { Id = 2, Name = "Operation room 2", Type = Room.RoomType.OperationRoom, PositionAndDimension = new PositionAndDimension(110, 100, 190, 110), FloorId = 1 });
-/*                context.Equipments.Add(new Equipment { Id = 1, Name = "Bed", Type = EquipmentType.Static, Quantity = 25, RoomId = 1 });
-                context.Equipments.Add(new Equipment { Id = 2, Name = "TV", Type = EquipmentType.Static, Quantity = 2, RoomId = 2 });
-                context.Equipments.Add(new Equipment { Id = 3, Name = "TV", Type = EquipmentType.Static, Quantity = 25, RoomId = 1 });*/
-                context.Equipments.Add(new Equipment { Id = 4, Name = "Blanket", Type = EquipmentType.Dynamic, Quantity = 27, RoomId = 1 });
-/*                context.Equipments.Add(new Equipment { Id = 5, Name = "Needle", Type = EquipmentType.Dynamic, Quantity = 25, RoomId = 1 });*/
-                context.SaveChanges();
-            }
+            renovations.Add(new Renovation { Id = 1, FirstRoomId = 1, SecondRoomId = 2, Duration = 4, Date = renovationDate, Type = Renovation.RenovationType.Merge });
 
-            return options;
-        }
+            stubRepository.Setup(m => m.GetAll()).Returns(renovations);
 
-        private void ClearStubRepository(HospitalDbContext context)
-        {
-            foreach (Renovation re in context.Renovations)
-            {
-                context.Renovations.Remove(re);
-                context.SaveChanges();
-            }
-            foreach (Room r in context.Rooms)
-            {
-                context.Rooms.Remove(r);
-                context.SaveChanges();
-            }
-            foreach (Transfer t in context.Transfer)
-            {
-                context.Transfer.Remove(t);
-                context.SaveChanges();
-            }
-            foreach (Equipment e in context.Equipments)
-            {
-                context.Equipments.Remove(e);
-                context.SaveChanges();
-            }
+            return stubRepository.Object;
         }
 
         [Fact]
-        public void Check_available_free_term_for_renovation()
+        public void Get_available_free_term_for_renovation_when_free_terms_exist()
         {
-            var options = CreateStubRepository();
+            var renovationRepository = new Mock<IRenovationRepository>().Object;
+            var transferRepository = new Mock<ITransferRepository>().Object;
+            var appointmentRepository = new Mock<IAppointmentRepository>().Object;
+            RenovationService renovationService = new RenovationService(renovationRepository, transferRepository, appointmentRepository);
+            var renovationDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(3).Day, 8, 0, 0);
+            var availableDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(1).Day, 8, 0, 0);
+            Renovation renovation = new Renovation(4, 1, 2, renovationDate, 1, Renovation.RenovationType.Merge);
+            
+            List<DateTime> freeTerms = renovationService.getFreeTermsForMerge(renovation);
 
-            using (var context = new HospitalDbContext(options))
-            {
-                RenovationRepository renovationRepository = new RenovationRepository(context);
-                TransferRepository transferRepository = new TransferRepository(context);
-                AppointmentRepository appointmentRepository = new AppointmentRepository(context);
-                RenovationService renovationService = new RenovationService(renovationRepository, transferRepository, appointmentRepository);
-                Renovation renovation = new Renovation(4, 1, 2, new DateTime(2022, 8, 17, 14, 30, 00), 1, Renovation.RenovationType.Merge);
-                List<DateTime> freeTerms = renovationService.getFreeTermsForMerge(renovation);
-                ClearStubRepository(context);
+            Assert.Contains(availableDate, freeTerms);
+        }
 
+        [Fact]
+        public void Get_available_free_term_for_renovation_when_free_terms_not_exist()
+        {
+            var renovationRepository = CreateRenovationsStubRepository();
+            var transferRepository = new Mock<ITransferRepository>().Object;
+            var appointmentRepository = new Mock<IAppointmentRepository>().Object;
+            RenovationService renovationService = new RenovationService(renovationRepository, transferRepository, appointmentRepository);
+            var renovationDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.AddDays(2).Day, 8, 0, 0);
+            Renovation renovation = new Renovation(4, 1, 2, renovationDate, 1, Renovation.RenovationType.Merge);
 
-                Assert.Contains(new DateTime(2022, 8, 16, 8, 30, 0), freeTerms);
-            }
+            List<DateTime> freeTerms = renovationService.getFreeTermsForMerge(renovation);
+
+            Assert.Empty(freeTerms);
         }
 
 
@@ -95,70 +61,24 @@ namespace HospitalUnitTests.Graphical_editor
         [MemberData(nameof(Data))]
         public void Get_room_renovations(int roomId, int roomRenovationsCount)
         {
-            var options = CreateStubRepository();
+            var renovationRepository = CreateRenovationsStubRepository();
+            var transferRepository = new Mock<ITransferRepository>().Object;
+            var appointmentRepository = new Mock<IAppointmentRepository>().Object;
+            RenovationService renovationService = new RenovationService(renovationRepository, transferRepository, appointmentRepository);
 
-            using (var context = new HospitalDbContext(options))
-            {
-                RenovationRepository renovationRepository = new RenovationRepository(context);
-                TransferRepository transferRepository = new TransferRepository(context);
-                AppointmentRepository appointmentRepository = new AppointmentRepository(context);
-                RenovationService renovationService = new RenovationService(renovationRepository, transferRepository, appointmentRepository);
+            List<Renovation> roomRenovations = renovationService.GetRoomRenovations(roomId);
 
-                List<Renovation> roomRenovations = renovationService.GetRoomRenovations(roomId);
-                ClearStubRepository(context);
-
-
-
-                Assert.Equal(roomRenovationsCount, roomRenovations.Count);
-            }
+            Assert.Equal(roomRenovationsCount, roomRenovations.Count);
         }
 
         public static IEnumerable<object[]> Data()
         {
             var retVal = new List<object[]>();
 
-            retVal.Add(new object[] { 1, 2 });
+            retVal.Add(new object[] { 1, 1 });
             retVal.Add(new object[] { 3, 0 });
 
             return retVal;
-        }
-
-        [Fact]
-        public void Check_equipment_transfer_from_one_room_to_another()
-        {
-            var options = CreateStubRepository();
-
-            using (var context = new HospitalDbContext(options))
-            {
-                EquipmentRepository equipmentRepository = new EquipmentRepository(context);
-                EquipmentService equipmentService = new EquipmentService(equipmentRepository);
-
-                Equipment eq = new Equipment(4, "Blanket", EquipmentType.Dynamic, 27, 2);
-
-                List<Equipment> newEquipment = equipmentService.TransferEquipmentFromOneRoomToAnother(2, 1);
-
-                ClearStubRepository(context);
-
-                Assert.Contains(eq, newEquipment);
-            }
-        }
-
-        [Fact]
-        public void Check_equipment_quantity_when_transfered_from_one_room_to_another()
-        {
-            var options = CreateStubRepository();
-
-            using (var context = new HospitalDbContext(options))
-            {
-                EquipmentRepository equipmentRepository = new EquipmentRepository(context);
-                EquipmentService equipmentService = new EquipmentService(equipmentRepository);
-
-                int quantity = equipmentService.GetQuantityOfEquipmentWhenTransferedFromOneRoomToAnother("Blanket", 2, 1);
-
-                ClearStubRepository(context);
-
-                Assert.Equal(27, quantity);
-            }
         }
     }
 }
